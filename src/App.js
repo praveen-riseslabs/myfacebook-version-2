b6b8395453ae3342495025330cb10df298cf9ba4
SwarnalathaMadiki <swarnalatha.madiki@riseslabs.com>
2024-04-09 18:41:07 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -55,6 +55,41 @@ function App() {
     </BrowserRouter>
   );
 }
+import "../src/styles/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login } from './components/login.component';
+import Dashboard from "./pages/dashboard";
+import Documents from "./pages/documents";
+import Uielements from "./pages/uielements";
+import Formelements from "./pages/formelements";
+import Charts from "./pages/charts";
+import Tables from "./pages/tables";
+import Icons from "./pages/icons";
+import Userpages from "./pages/userpages";
+import Errorpages from "./pages/errorpages";
+import Sidebar from "./components/sidebar";
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Sidebar />
+                <Routes>
+                    <Route path='/login' element={<Login></Login>}></Route>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/documents" element={<Documents />} />
+                    <Route path="/uielements" element={<Uielements />} />
+                    <Route path="/formelements" element={<Formelements />} />
+                    <Route path="/charts" element={<Charts />} />
+                    <Route path="/tables" element={<Tables />} />
+                    <Route path="/icons" element={<Icons/>}/>
+                    <Route path="/users" element={<Userpages />} />
+                    <Route path="/error" element={<Errorpages />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
 
 export default App;
 
